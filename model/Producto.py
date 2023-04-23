3fbc9c39007119113c5d1247c39d60b00bce9727
NataliaPerez08 <natalia_perez08@comunidad.unam.mx>
2023-04-22 19:28:28 -0600

    Inicio del proyecto

---
+++ model/Producto.py
@@ -0,0 +1,5 @@
+class Producto:
+    def __init__(self, id, nombre, descripcion):
+        self.id = id
+        self.nombre = nombre
+        self.descripcion = descripcion
---
+++ views/index.html
@@ -0,0 +1,3 @@
+<html>
+    <h1>Hola</h1>
+</html>
