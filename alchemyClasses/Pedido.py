8254be650a0cc852192b4c8d963c6feabb7d62a3
Cecilia del Carmen <>
2023-05-13 18:47:01 -0600

    Cambios para relacionarlos a la base de datos.

--- alchemyClasses/Pedido.py
+++ alchemyClasses/Pedido.py
@@ -1,8 +1,13 @@
+from alchemyClasses.__init__ import db
 from Producto import Producto
 from Usuario import Usuario
 from models.InfoPago import InfoPago
 import datetime
-class Pedido:
+class Pedido(db.Model):
+    '''
+    create table pedido (idPedido int not null auto_increment primary key, cantidad int not null,
+                     total decimal(6,2) NOT NULL DEFAULT '9999.99', estatus boolean, fecha date);
+    '''
     def __init__(self, idPedido, producto:Producto, cantidad, comprador:Usuario, total, estatus, infoPago:InfoPago):
         self.idPedido = idPedido
         self.producto = producto
