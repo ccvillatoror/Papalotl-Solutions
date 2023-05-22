8c5ac3ff22da2c8d1f1c2ef2f2b72b63e625d665
Saul-Tlahuiz <126300914+Saul-Tlahuiz@users.noreply.github.com>
2023-05-22 15:58:51 -0600

    Update insumo.py

--- alchemyClasses/insumo.py
+++ alchemyClasses/insumo.py
@@ -2,7 +2,7 @@ from alchemyClasses._init_import db
 
 class insumo(db.Model):
   id_insumo = db.Column('idInsumo', db.Integer, primary_key = True) #int auto_increment.
-  nombre = db.Column('nombre', db.String(25), nullable = False)
+  nombre = db.Column('nombre', db.String(25), nullable = True)
   cantidad = db.Column('cantidad', db.Integer, nullable = False)
   fecha = db.Column('fecha', db.DateTime, nullable = False)
   
