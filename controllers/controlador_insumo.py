5f9fa7f3f9fed57b6ee519178527bc5ac898a83b
Saul-Tlahuiz <126300914+Saul-Tlahuiz@users.noreply.github.com>
2023-05-22 16:02:59 -0600

    Update controlador_insumo.py

--- controllers/controlador_insumo.py
+++ controllers/controlador_insumo.py
@@ -1,4 +1,4 @@
-from flask import Blueprint , flash , g , redirect , render_template , request , session , url_fo
+from flask import Blueprint , flash , g , redirect , render_template , request , session , url_for
 from alchemyClasses.insumo import insumo
 from models.modelo_insumo import insumo, consultar_insumo
 
