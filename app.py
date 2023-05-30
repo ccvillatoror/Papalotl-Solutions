c54f8db8f34841b9aaf55c99ca5fd0c42bf93c9d
NataliaPerez08 <natalia_perez08@comunidad.unam.mx>
2023-05-29 21:54:01 -0600

    Mejor manejo del Modelo Vista Controlador. Se agrega controlador de producto y modelo de producto

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 from controllers.ControladorUsuario import registro_cliente_blueprint
 
+from controllers.ControladorProducto import consulta_productos_blueprint
 
 
 DATABASE_NAME = "micheladasatucasa"
@@ -16,7 +17,8 @@ DATABASE_HOST = "localhost:3306"
 app = Flask(__name__)
 app.register_blueprint(registro_cliente_blueprint)
 app.register_blueprint(loginBlueprint)
-#app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://natalia:ati_desa15@localhost:3306/prueba"
+
+app.register_blueprint(consulta_productos_blueprint)
 
 app.config[
     'SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + DATABASE_USERNAME + ':' + DATABASE_PASSWORD + '@' + DATABASE_HOST + '/' + DATABASE_NAME
@@ -24,8 +26,6 @@ app.config.from_mapping(
     SECRET_KEY='dev'
 )
 
-#db = SQLAlchemy()
-#db.init_app(app)
 def create_app():
     db.init_app(app)
     return app
@@ -39,97 +39,45 @@ with app.app_context():
     except Exception as e:
         print('\n\n----------- Connection failed ! ERROR : ', e)
 
-
 # ---------------------------
-
-@app.route('/editarP/<int:idProducto>', methods=['GET'])
-def modificar_producto(idProducto):
-    producto = Producto.query.get(idProducto)
-    if request.method == 'GET':
-        nombre = request.args.get('nombre')
-        des = request.args.get('descripcion')
-        precio = request.args.get('precio')
-        cant = request.args.get('cant_inventario')
-
-        producto.nombre = nombre
-        producto.descripcion = des
-        producto.precio = precio
-        producto.cant_inventario = cant
-        db.session.commit()
-        return redirect('/productos')
-       
-
 @app.route('/editarProducto/<int:idProducto>', methods=['GET'])
-def mostrar_modificar_producto(idProducto):
-    query = text('SELECT * FROM producto WHERE idProducto = :idProducto')
-    producto = db.session.execute(query, {'idProducto':idProducto})
-    producto = producto.fetchone()
-    return render_template('form_editar_producto.html', producto=producto)
-
+def editar_producto(idProducto):
+    return redirect(url_for('consulta_productos_blueprint.editar_producto', idProducto=idProducto))
 
 # ---------------------------
-@app.route('/registrarProducto')
-def mostrar_registro_producto():
-    return render_template("form_registrar_producto.html")
-
-
-@app.route('/reProducto', methods=['GET'])
-def registro_produto():
-    if request.method == 'GET':
-        nombre = request.args.get('nombre')
-        des = request.args.get('descripcion')
-        precio = request.args.get('precio')
-        cant = request.args.get('cant_inventario')
-        error = None
-
-        producto = Producto(cant, nombre, des, precio)
-        if not nombre:
-            error = 'Se requiere nombre'
-        elif not precio:
-            error = 'Se requiere precio'
-        db.session.add(producto)
-        db.session.commit()
-    return redirect('/productos')
-
+@app.route('/registrarProducto', methods=['GET', 'POST'])
+def registro_producto():
+    return redirect(url_for('consulta_productos_blueprint.agregar_producto'))
 
 # ---------------------------
 @app.route('/productos')
 def mostrar_productos():
-    productos = db.session.execute(text('SELECT * FROM producto'))
-    return render_template('productos.html', productos=productos)
-
+    return redirect(url_for('consulta_productos_blueprint.productos'))
 
 # ---------------------------
 @app.route('/producto/<int:idProducto>')
 def mostrar_producto(idProducto):
-    query = text('SELECT * FROM producto WHERE idProducto = :idProducto')
-    producto = db.session.execute(query, {'idProducto':idProducto})
-    producto = producto.fetchone()
-    if producto is None:
-        return render_template('errorProducto.html')
-    return render_template('producto.html', producto=producto)
-
+    return redirect(url_for('consulta_productos_blueprint.mostrar_producto', idProducto=idProducto))
 
 # ---------------------------
-@app.route('/eliminarProducto/<int:idProducto>', methods=['GET'])
+@app.route('/eliminarProducto/<int:idProducto>')
 def eliminar_producto(idProducto):
-    producto = Producto.query.get(idProducto)
-    #db.session.delete(producto)
-    #db.session.commit()
-    return redirect('/productos')
+    return redirect(url_for('consulta_productos_blueprint.eliminar_producto', idProducto=idProducto))
 
+# ---------------------------
 @app.route("/")
 def home():
     productos = Producto.query.filter().all()
     return render_template("casa.html", productos=productos)
 
+# ---------------------------
 @app.route("/login", methods=["GET","POST"])
 def login():
     if request.method == "POST":
         return redirect(url_for("login.login"))
     else:
         return render_template("login.html")
-
+# ---------------------------
 @app.route("/registro-cliente", methods=["GET","POST"])
 def registro_cliente():
     if request.method == "POST":
@@ -137,6 +85,7 @@ def registro_cliente():
     else:
         return render_template("registro_cliente.html")
 
+# ---------------------------
 @app.route("/<usr>")
 def user(usr):
 
